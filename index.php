e4d7a6df591e08989c8257dff88f8c716206f7cb
Wagner Medina <80843424+wamedina-dev@users.noreply.github.com>
2025-06-29 11:28:30 -0300

    Upload files inicial
    Envio dos arquivos iniciais para criação do repositório

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Provador Virtual</title>
+    <meta name="pragma" content="no-cache" />
+    <meta name="cache-control" content="Public" />
+    <meta name="expires" content="0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body style="margin:0 auto; width:100%; max-width: 890px;">
+  
+    <div id="corpo"></div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+      $("#corpo").load("ajax/init-geral.php?prod=<?=@$_GET['prod']?>", function () {});
+    </script>
+  </body>
+</html>
+
+
+
